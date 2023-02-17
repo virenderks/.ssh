4399dd4b1401420f9aaeea6085433e6d2caaf22e
virenderks <veersharma898@gmail.com>
2023-02-17 15:11:41 +0530

    promisescommit

--- share/code-server/promises2.js
+++ share/code-server/promises2.js
@@ -1,28 +1,41 @@
-function getButter() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('butter');
-      }, 2000);
+ 
+<button onclick="createPost()">Create Post</button>
+<button onclick="deletePost()">Delete Post</button>
+
+
+const posts = [];
+
+async function createPost() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
     });
+    const data = await response.json();
+    posts.push(data);
+    console.log('Post created:', data);
+  } catch (error) {
+    console.error(error);
   }
-  
-  function getColdDrinks() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve('cold drinks');
-      }, 1000);
+}
+
+async function deletePost() {
+  try {
+    const postId = posts[0].id; 
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+      method: 'DELETE'
     });
+    const data = await response.json();
+    posts.shift();
+    console.log('Post deleted:', data);
+  } catch (error) {
+    console.error(error);
   }
-  
-  getButter()
-    .then((butter) => {
-      console.log(`Husband got ${butter}`);
-      return getColdDrinks();
-    })
-    .then((drinks) => {
-      console.log(`Husband got ${drinks}`);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  
+}
